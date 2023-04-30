1dea5547bd13dcefd5ed7e431f072b3fa2a51c1b
noahlinton32 <noahlinton@gmail.com>
2023-04-29 23:09:00 -0400

    created form validation for editReferral

--- client/src/pages/editIncident.js
+++ client/src/pages/editIncident.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const EditIncident = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-
+  const [errors, setErrors] = useState({});
   const [form, setForm] = useState({
     incidentReportNumber: '',
     studentID: '',
@@ -33,6 +33,50 @@ const EditIncident = () => {
     const date = new Date(dateString);
     return date.toISOString().split('T')[0];
   };
+
+  const validateForm = (form) => {
+    const errors = {};
+
+    for (const key in validationRules) {
+      const rule = validationRules[key];
+      const value = form[key];
+
+      if (rule.required && (value === null || value === '')) {
+        errors[key] = `${key} is required`;
+      } else if (rule.type && typeof value !== rule.type) {
+        errors[key] = `${key} must be a ${rule.type}`;
+      } else if (rule.min !== undefined && value < rule.min) {
+        errors[key] = `${key} must be at least ${rule.min}`;
+      } else if (rule.max !== undefined && value > rule.max) {
+        errors[key] = `${key} must be at most ${rule.max}`;
+      }
+    }
+
+    return errors;
+  };
+  const validationRules = {
+    incidentReportNumber: { required: true, type: 'number' },
+    studentID: { required: true, type: 'number' },
+    agencyOrProgram: { required: true, type: 'string' },
+    contactNumber: { required: true, type: 'number' },
+    address: { required: true, type: 'string' },
+    incidentType: { required: true, type: 'string' },
+    incidentTypeOther: { required: false, type: 'string' },
+    location: { required: true, type: 'string' },
+    date: { required: true, type: 'string' },
+    employeeID: { required: true, type: 'number' },
+    firstNameParticipant: { required: true, type: 'string' },
+    lastNameParticipant: { required: true, type: 'string' },
+    firstNameReport: { required: true, type: 'string' },
+    lastNameReport: { required: true, type: 'string' },
+    contactReportNumber: { required: true, type: 'number' },
+    contactReportEmail: { required: true, type: 'string' },
+    firstNameManager: { required: true, type: 'string' },
+    lastNameManager: { required: true, type: 'string' },
+    phoneNumberManager: { required: true, type: 'number' },
+    emailAddressManager: { required: true, type: 'string' },
+    signed: { required: true, type: 'number', min: 0, max: 1 },
+  };
   useEffect(() => {
     const getIncident = async () => {
       try {
@@ -46,7 +90,6 @@ const EditIncident = () => {
     };
     getIncident();
   }, [id]);
-
   const removeCircularReferences = (obj) => {
     const seen = new WeakSet();
     return JSON.parse(JSON.stringify(obj, (key, value) => {
@@ -59,205 +102,272 @@ const EditIncident = () => {
       return value;
     }));
   };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const formWithoutCircularReferences = removeCircularReferences(form);
-    await axios.put(`http://localhost:3000/incidents/${id}`, formWithoutCircularReferences);
-    navigate('/incidents');
-  };
 
+    const errors = validateForm(form);
+    const hasErrors = Object.keys(errors).length > 0;
+
+    if (hasErrors) {
+      console.error("Form validation errors:", errors);
+      setErrors(errors);
+    } else {
+      const formWithoutCircularReferences = removeCircularReferences(form);
+      await axios.put(`http://localhost:3000/incidents/${id}`, formWithoutCircularReferences);
+      navigate('/incidents');
+    }
+  };
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
   return (
-          <div>
-            <h1>Edit Incident</h1>
-            <form onSubmit={handleSubmit}>
-        <label htmlFor="incidentReportNumber">Incident Report Number:</label>
+    <div>
+      <h1>Edit Incident</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="incidentReportNumber">Incident Report Number</label>
         <input
           type="number"
+          id="incidentReportNumber"
           name="incidentReportNumber"
           value={form.incidentReportNumber}
-          onChange={(event) => setForm({ ...form, incidentReportNumber: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.incidentReportNumber && (
+          <p className="error">{errors.incidentReportNumber}</p>
+        )}
 
-        <label htmlFor="studentID">Student ID:</label>
+        <label htmlFor="studentID">Student ID</label>
         <input
           type="number"
+          id="studentID"
           name="studentID"
           value={form.studentID}
-          onChange={(event) => setForm({ ...form, studentID: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.studentID && <p className="error">{errors.studentID}</p>}
 
-        <label htmlFor="agencyOrProgram">Agency or Program:</label>
+        <label htmlFor="agencyOrProgram">Agency or Program</label>
         <input
           type="text"
+          id="agencyOrProgram"
           name="agencyOrProgram"
           value={form.agencyOrProgram}
-          onChange={(event) => setForm({ ...form, agencyOrProgram: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.agencyOrProgram && (
+          <p className="error">{errors.agencyOrProgram}</p>
+        )}
 
-        <label htmlFor="contactNumber">Contact Number:</label>
-        <input
-          type="text"
-          name="contactNumber"
-          value={form.contactNumber}
-          onChange={(event) => setForm({ ...form, contactNumber: event.target.value })}
-        />
-        <br />
-
-        <label htmlFor="address">Address:</label>
-        <input
-          type="text"
-          name="address"
-          value={form.address}
-          onChange={(event) => setForm({ ...form, address: event.target.value })}
-        />
-        <br />
-
-        <label htmlFor="incidentType">Incident Type:</label>
-        <input
-          type="text"
-          name="incidentType"
-          value={form.incidentType}
-          onChange={(event) => setForm({ ...form, incidentType: event.target.value })}
-        />
-        <br />
-
-        <label htmlFor="incidentTypeOther">Incident Type (Other):</label>
-        <input
-          type="text"
-          name="incidentTypeOther"
-          value={form.incidentTypeOther}
-          onChange={(event) => setForm({ ...form, incidentTypeOther: event.target.value })}
-        />
-        <br />
-
-        <label htmlFor="location">Location:</label>
-        <input
-          type="text"
-          name="location"
-          value={form.location}
-          onChange={(event) => setForm({ ...form, location: event.target.value })}
-        />
-        <br />
-        <label htmlFor="date">Date:</label>
-        <input
-          type="date"
-          name="date"
-          value={form.date}
-          onChange={(event) => setForm({ ...form, date: event.target.value })}
-        />
-        <br />
-
-        <label htmlFor="employeeID">Employee ID:</label>
+        <label htmlFor="contactNumber">Contact Number</label>
         <input
           type="number"
+          id="contactNumber"
+          name="contactNumber"
+          value={form.contactNumber}
+          onChange={handleChange}
+        />
+        {errors.contactNumber && (
+          <p className="error">{errors.contactNumber}</p>
+        )}
+
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          id="address"
+          name="address"
+          value={form.address}
+          onChange={handleChange}
+        />
+        {errors.address && <p className="error">{errors.address}</p>}
+
+        <label htmlFor="incidentType">Incident Type</label>
+        <input
+          type="text"
+          id="incidentType"
+          name="incidentType"
+          value={form.incidentType}
+          onChange={handleChange}
+        />
+        {errors.incidentType && (
+          <p className="error">{errors.incidentType}</p>
+        )}
+
+        <label htmlFor="incidentTypeOther">Incident Type (Other)</label>
+        <input
+          type="text"
+          id="incidentTypeOther"
+          name="incidentTypeOther"
+          value={form.incidentTypeOther}
+          onChange={handleChange}
+        />
+        {errors.incidentTypeOther && (
+          <p className="error">{errors.incidentTypeOther}</p>
+        )}
+
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          value={form.location}
+          onChange={handleChange}
+        />
+        {errors.location && <p className="error">{errors.location}</p>}
+
+        <label htmlFor="date">Date</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={form.date}
+          onChange={handleChange}
+        />
+        {errors.date && <p className="error">{errors.date}</p>}
+        <label htmlFor="employeeID">Employee ID</label>
+        <input
+          type="number"
+          id="employeeID"
           name="employeeID"
           value={form.employeeID}
-          onChange={(event) => setForm({ ...form, employeeID: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.employeeID && <p className="error">{errors.employeeID}</p>}
 
-        <label htmlFor="firstNameParticipant">First Name (Participant):</label>
+        <label htmlFor="firstNameParticipant">First Name (Participant)</label>
         <input
           type="text"
+          id="firstNameParticipant"
           name="firstNameParticipant"
           value={form.firstNameParticipant}
-          onChange={(event) => setForm({ ...form, firstNameParticipant: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.firstNameParticipant && (
+          <p className="error">{errors.firstNameParticipant}</p>
+        )}
 
-        <label htmlFor="lastNameParticipant">Last Name (Participant):</label>
+        <label htmlFor="lastNameParticipant">Last Name (Participant)</label>
         <input
           type="text"
+          id="lastNameParticipant"
           name="lastNameParticipant"
           value={form.lastNameParticipant}
-          onChange={(event) => setForm({ ...form, lastNameParticipant: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
-        <label htmlFor="firstNameReport">First Name (Report):</label>
+        {errors.lastNameParticipant && (
+          <p className="error">{errors.lastNameParticipant}</p>
+        )}
+
+        <label htmlFor="firstNameReport">First Name (Report)</label>
         <input
           type="text"
+          id="firstNameReport"
           name="firstNameReport"
           value={form.firstNameReport}
-          onChange={(event) => setForm({ ...form, firstNameReport: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.firstNameReport && (
+          <p className="error">{errors.firstNameReport}</p>
+        )}
 
-        <label htmlFor="lastNameReport">Last Name (Report):</label>
+        <label htmlFor="lastNameReport">Last Name (Report)</label>
         <input
           type="text"
+          id="lastNameReport"
           name="lastNameReport"
           value={form.lastNameReport}
-          onChange={(event) => setForm({ ...form, lastNameReport: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.lastNameReport && (
+          <p className="error">{errors.lastNameReport}</p>
+        )}
 
-        <label htmlFor="contactReportNumber">Contact Number (Report):</label>
+        <label htmlFor="contactReportNumber">Contact Report Number</label>
         <input
-          type="text"
+          type="number"
+          id="contactReportNumber"
           name="contactReportNumber"
           value={form.contactReportNumber}
-          onChange={(event) => setForm({ ...form, contactReportNumber: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.contactReportNumber && (
+          <p className="error">{errors.contactReportNumber}</p>
+        )}
 
-        <label htmlFor="contactReportEmail">Email (Report):</label>
+        <label htmlFor="contactReportEmail">Contact Report Email</label>
         <input
           type="email"
+          id="contactReportEmail"
           name="contactReportEmail"
           value={form.contactReportEmail}
-          onChange={(event) => setForm({ ...form, contactReportEmail: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.contactReportEmail && (
+          <p className="error">{errors.contactReportEmail}</p>
+        )}
 
-        <label htmlFor="firstNameManager">First Name (Manager):</label>
+        <label htmlFor="firstNameManager">First Name (Manager)</label>
         <input
           type="text"
+          id="firstNameManager"
           name="firstNameManager"
           value={form.firstNameManager}
-          onChange={(event) => setForm({ ...form, firstNameManager: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.firstNameManager && (
+          <p className="error">{errors.firstNameManager}</p>
+        )}
 
-        <label htmlFor="lastNameManager">Last Name (Manager):</label>
+        <label htmlFor="lastNameManager">Last Name (Manager)</label>
         <input
           type="text"
+          id="lastNameManager"
           name="lastNameManager"
           value={form.lastNameManager}
-          onChange={(event) => setForm({ ...form, lastNameManager: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.lastNameManager && (
+          <p className="error">{errors.lastNameManager}</p>
+        )}
 
-        <label htmlFor="phoneNumberManager">Phone Number (Manager):</label>
+        <label htmlFor="phoneNumberManager">Phone Number (Manager)</label>
         <input
-          type="text"
+          type="number"
+          id="phoneNumberManager"
           name="phoneNumberManager"
           value={form.phoneNumberManager}
-          onChange={(event) => setForm({ ...form, phoneNumberManager: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
-
-        <label htmlFor="emailAddressManager">Email (Manager):</label>
+        {errors.phoneNumberManager && (
+          <p className="error">{errors.phoneNumberManager}</p>
+        )}
+        <label htmlFor="emailAddressManager">Email Address (Manager)</label>
         <input
           type="email"
+          id="emailAddressManager"
           name="emailAddressManager"
           value={form.emailAddressManager}
-          onChange={(event) => setForm({ ...form, emailAddressManager: event.target.value })}
+          onChange={handleChange}
         />
-        <br />
+        {errors.emailAddressManager && (
+          <p className="error">{errors.emailAddressManager}</p>
+        )}
 
-        <label htmlFor="signed">Signed:</label>
+        <label htmlFor="signed">Signed</label>
         <input
-          type="text"
+          type="checkbox"
+          id="signed"
           name="signed"
-          value={form.signed}
-          onChange={(event) => setForm({ ...form, signed: event.target.value })}
+          checked={form.signed}
+          onChange={(event) =>
+            setForm({ ...form, signed: event.target.checked })
+          }
         />
-        <br />
+        {errors.signed && <p className="error">{errors.signed}</p>}
 
-        <button type="submit">Submit</button>
+        <button type="submit">Update Incident</button>
       </form>
     </div>
   );
