3974f0cccccb8d3370edb9677ad8786d24940b7d
JuanJimene <n01375595@unf.edu>
2023-04-30 22:41:41 -0400

    Added table functionality for students view

--- client/src/pages/students.js
+++ client/src/pages/students.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from "react-bootstrap";
 import styled, {createGlobalStyle, css} from 'styled-components';
 import Collapsible from 'react-collapsible';
+import { MDBDataTable } from 'mdbreact';
 
 const GlobalStyle = createGlobalStyle`
   html{
@@ -38,67 +39,86 @@ function Students() {
     const res = await axios.get('http://localhost:3000/students');
     setStudents(res.data.students);
   };
+  for (let i in students){
+    students[i]['edit']=(<NavLink to={`/students/edit/${students[i]['_id']}`}><button style={{backgroundColor: 'lightblue'}}>Edit</button></NavLink>)
+    students[i]['guardian']= students[i]['firstNameFirstGuardian']+' '+students[i]['lastNameFirstGuardian']
+    students[i]['guardian2']= students[i]['firstNameSecondGuardian']+' '+students[i]['lastNameSecondGuardian']
+
+  }
+  const data = {
+    columns: [
+      {
+        label: 'Name',
+        field: 'name',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Student ID',
+        field: 'studentID',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'GPA',
+        field: 'gpa',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Grade',
+        field: 'grade',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Emergency Number',
+        field: 'emergencyNumber',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Guardian',
+        field: 'guardian',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Secondary Guardian',
+        field: 'guardian2',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Edit',
+        field: 'edit',
+      }
+    ],
+    rows: 
+      students
+    }
+    console.log(data)
   
   
-  return <div style={{marginLeft: '45%'}}>
+  return <div style={{margin: 'auto'}}>
     <GlobalStyle/>
-    <h2>Students</h2>
-    <div style={{display: 'grid', marginLeft: '-45%'}}>
-    {students && students.map (student => {
-        return <div key={student._id}> 
-
-<td>
-    {student.isActive === 1 ? 
-    <div style={{backgroundColor: '#fff',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: '2%',
-                height: '100%',
-                width: '100%',
-                maxWidth: '1000px',
-                padding: '0 20px',
-                borderRadius: '10px',
-                boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.2)'
-                }}>
-    <Collapsible trigger={<h5>{student.name}</h5>} style={{backgroundColor: '#fff'}}>
-      <div style={{
-        display: 'grid',
-        width:'100%',
-        gridTemplateRows: '40px 40px 40px 60px 10px 30px',
-        gridTemplateColumns: '400px 400px',
-
-
-      }}>
-    <p>Student ID: {student.studentID}</p>
-    <p>Address: {student.address}</p>
-    <p>GPA: {student.gpa}</p>
-    <p>Grade: {student.grade}</p>
-    <p>Guardian 1: {student.firstNameFirstGuardian} {student.lastNameFirstGuardian}</p>
-    <p>Guardian 2: {student.firstNameSecondGuardian} {student.lastNameSecondGuardian}</p>
-    <p>Emergency Number: {student.emergencyNumber}</p>
-    <p>Enrollment Date: {student.enrollmentDate.split('T')[0]}</p> 
-    <div>
-    <NavLink to={`/students/edit/${student._id}`}>
-    <Button style={{backgroundColor: '#D1913C', color:'#000', border: '0'}}>Edit Student</Button>
-    </NavLink>
-
-    </div> 
-      </div>
-  </Collapsible>
-  </div>
-  :null}
-</td>
-</div>
-    })}
-</div> 
-
-
-<div style={{marginLeft:'-2%', marginTop: '2%'}}>
+    <h2 style={{marginLeft: '50%'}}>Students</h2>
+    <div style={{width: '80%', marginLeft:'10%', backgroundColor: '#fff',
+                borderRadius: '10px', padding: '0 20px',boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.2)', minWidth: '650px'}}>
+    <MDBDataTable
+      striped
+      small
+      data={data}
+      noBottomColumns={true} 
+    />
+    <div style={{ paddingBottom:'20px'}}>
 <NavLink to="/students/create" activeStyle>
     <Button>Create Student</Button>
 </NavLink>
 </div>
+    </div>
+
+
 </div>     
 }
 export default Students;
