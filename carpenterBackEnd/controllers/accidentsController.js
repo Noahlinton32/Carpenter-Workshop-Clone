0c8723fba775716db994aeb2d971bd66f1416b25
noahlinton32 <noahlinton@gmail.com>
2023-05-02 08:57:37 -0400

    also updated controller

--- carpenterBackEnd/controllers/accidentsController.js
+++ carpenterBackEnd/controllers/accidentsController.js
@@ -8,15 +8,6 @@ const getAllAccidents = async (req, res)=> {
     res.json({accidents:accidents});
 };
 
-const getOneAccident = async (req, res) => {
-    //Get id from URL
-    const reportID = req.params.id;
-
-    //Find accident by ID (Report Num)
-    const accident = await Accident.findOne({accidentReportNumber: reportID});
-    //respond with accident
-    res.json({accident:accident});
-};
 
 const createAccident =  async (req, res) => {
     //Get data from req body
@@ -58,63 +49,49 @@ const createAccident =  async (req, res) => {
     res.json({accident: accident})
 };
 
-const updateAccident = async (req, res) =>{
-    //get id
-    const reportID = req.params.id;
-
-    //get data off req body 
-    const accidentReportNumber = req.body.accidentReportNumber;
-    const studentID = req.body.studentID;
-    const school = req.body.school;
-    const employeeID = req.body.employeeID;
-    const room = req.body.room;
-    const date = req.body.date;
-    const location = req.body.location;
-    const employeeIDInvolved = req.body.employeeIDInvolved;
-    const studentIDInvolved = req.body.studentIDInvolved;
-    const cause = req.body.cause;
-    const response = req.body.response;
-    const preventativeAction = req.body.preventativeAction;
-    const witnesses = req.body.witnesses;
-    const signed = req.body.signed;
-
-    //find and update
-        await Accident.findOneAndUpdate({accidentReportNumber: reportID}, {
-            accidentReportNumber: accidentReportNumber,
-            studentID: studentID,
-            school: school,
-            employeeID: employeeID,       
-            room: room,
-            date: date, 
-            location: location,
-            employeeIDInvolved: employeeIDInvolved,
-            studentIDInvolved: studentIDInvolved,
-            cause: cause,
-            response: response,
-            preventativeAction: preventativeAction,
-            witnesses: witnesses,
-            signed: signed,
-    });
-    //find updated accident
-    const accident = await Accident.findOne({accidentReportNumber: reportID});
-    //respond
-    res.json({accident:accident})
+const updateAccident = async (req, res) => {
+  try{
+  const { id } = req.params;
+  const updatedAccident = await Accident.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.json({ accident: updatedAccident });
+}catch(error){
+  res.status(500).json({ error: "An error occurred while updating the incident." });
+  }
+  
 };
 
-//SETUP DELETE CONTROLLER AND ROUTE IN SERVER.JS
-const deleteAccident = async (req,res) => {
-    //find accident to delete
+const getAccidentById = async (req, res) => {
+  try {
+    const accident = await Accident.findById(req.params.id);
+    if (!accident) {
+      return res.status(404).json({ message: 'Accident not found' });
+    }
+    res.json({ accident });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
-    //delete accident
-
-    //respond with deleted accident
-
-}
+const deleteAccident = async (req, res) => {
+  try {
+    const accident = await Accident.findByIdAndDelete(req.params.id);
+    if (!accident) {
+      res.status(404).json({ message: 'Accident not found' });
+    } else {
+      res.status(200).json({ message: 'Accident deleted successfully', accident: accident });
+    }
+  } catch (error) {
+    console.error('Error deleting accident:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
 module.exports = {
     getAllAccidents: getAllAccidents,
-    getOneAccident: getOneAccident,
     createAccident: createAccident,
     updateAccident: updateAccident,
-    deleteAccident: deleteAccident
+    deleteAccident: deleteAccident,
+    getAccidentById:getAccidentById,
 }
--- carpenterBackEnd/controllers/studentsController.js
+++ carpenterBackEnd/controllers/studentsController.js
@@ -8,16 +8,6 @@ const getAllStudents = async (req, res)=> {
     res.json({students:students});
 };
 
-const getOneStudent = async (req, res) => {
-    //Get id from URL
-    const studID = req.params.id;
-
-    //Find student by ID
-    const student = await Student.findOne({studentID: studID});
-    //respond with student
-    res.json({student:student});
-};
-
 const createStudent =  async (req, res) => {
     //Get data from req body
     const studentID = req.body.studentID;
@@ -54,52 +44,36 @@ const createStudent =  async (req, res) => {
     res.json({student: student})
 };
 
-const updateStudent = async (req, res) =>{
-    //get id
-    const studID = req.params.id;
+const updateStudent = async (req, res) => {
+  const id = req.params.id;
+  const updatedStudent = req.body;
 
-    //get data off req body 
-    const studentID = req.body.studentID;
-    const name = req.body.name;
-    const address = req.body.address;
-    const gpa = req.body.gpa;
-    const grade = req.body.grade;
-    const firstNameFirstGuardian = req.body.firstNameFirstGuardian;
-    const lastNameFirstGuardian = req.body.lastNameFirstGuardian;
-    const firstNameSecondGuardian = req.body.firstNameSecondGuardian;
-    const lastNameSecondGuardian = req.body.lastNameSecondGuardian;
-    const emergencyNumber = req.body.emergencyNumber;
-    const enrollmentDate = req.body.enrollmentDate;
-    const graduationDate = req.body.graduationDate;
-    const isActive = req.body.isActive;
-
-    //find and update
-        await Student.findOneAndUpdate({studentID: studID}, {
-        studentID: studentID,
-        name: name,
-        address: address,
-        gpa: gpa,
-        grade: grade,
-        firstNameFirstGuardian: firstNameFirstGuardian, 
-        lastNameFirstGuardian: lastNameFirstGuardian, 
-        firstNameSecondGuardian: firstNameSecondGuardian,
-        lastNameSecondGuardian: lastNameSecondGuardian,
-        emergencyNumber: emergencyNumber,
-        enrollmentDate: enrollmentDate,
-        graduationDate: graduationDate,
-        isActive: isActive,
-
-    });
-    //find updated student
-    const student = await Student.findOne({studentID: studID});
-    //respond
-    res.json({student:student})
+  try {
+      const result = await Student.findByIdAndUpdate(id, updatedStudent, { new: true });
+      res.status(200).json({ student: result });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
 };
 
+const getStudentById = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+      const student = await Student.findById(id);
+      if (!student) {
+          res.status(404).json({ message: 'Student not found' });
+      } else {
+          res.status(200).json({ student });
+      }
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
     getAllStudents: getAllStudents,
-    getOneStudent: getOneStudent,
     createStudent: createStudent,
-    updateStudent: updateStudent
+    updateStudent: updateStudent,
+    getStudentById: getStudentById,
 }
