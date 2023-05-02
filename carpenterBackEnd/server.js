760f18cec5b065825c66913ff80de47e936c506c
noahlinton32 <noahlinton@gmail.com>
2023-05-02 08:21:27 -0400

    LETS GO WORKING SERVER

--- carpenterBackEnd/server.js
+++ carpenterBackEnd/server.js
@@ -22,7 +22,12 @@ dbconnection();
 
 //Configure express app
 app.use(express.json()); 
-app.use(cors());
+app.use(cors({
+  origin: ['https://carpenterservice.onrender.com', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.static(frontendBuildPath));
 //Routes
 
