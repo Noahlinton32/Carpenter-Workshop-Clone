7292909d61cf704f4ed2daae84939c95e0654949
noahlinton32 <noahlinton@gmail.com>
2023-05-03 23:39:17 -0400

    add archive students

--- carpenterFrontEnd/src/pages/students.js
+++ carpenterFrontEnd/src/pages/students.js
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import {NavLink} from "../components/Navbar/NavbarElements";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from "../components/Navbar/NavbarElements";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
-import styled, {createGlobalStyle, css} from 'styled-components';
-import Collapsible from 'react-collapsible';
+import styled, { createGlobalStyle, css } from "styled-components";
+import Collapsible from "react-collapsible";
 
 const GlobalStyle = createGlobalStyle`
   html{
     height: 100%;
-
   }
 
   body{
@@ -20,87 +19,144 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     color: 555;
   }
-`
-
-
+`;
 
 function Students() {
   // State
   const [students, setStudents] = useState(null);
-  
-  //User Effect
-  useEffect (() => {
+
+  // Use Effect
+  useEffect(() => {
     getStudents();
   }, []);
 
-  //Functions 
+  // Functions
   const getStudents = async () => {
-    //get students
-    const res = await axios.get('https://carpenterservice.onrender.com/students');
-    //set state
+    // Get students
+    const res = await axios.get("https://carpenterservice.onrender.com/students");
+    // Set state
     setStudents(res.data.students);
   };
-  
-  
-  return <div style={{marginLeft: '45%'}}>
-    <GlobalStyle/>
-    <h2>Students</h2>
-    <div style={{display: 'grid', marginLeft: '-45%'}}>
-    {students && students.map (student => {
-        return <div key={student.id}> 
 
-<td>
-    {student.isActive === 1 ? 
-    <div style={{backgroundColor: '#fff',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: '2%',
-                height: '100%',
-                width: '100%',
-                maxWidth: '1000px',
-                padding: '0 20px',
-                borderRadius: '10px',
-                boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.2)'
-                }}>
-    <Collapsible trigger={<h5>{student.name}</h5>} style={{backgroundColor: '#fff'}}>
-      <div style={{
-        display: 'grid',
-        width:'100%',
-        gridTemplateRows: '40px 40px 40px 60px 10px 30px',
-        gridTemplateColumns: '400px 400px',
-
-
-      }}>
-    <p>Student ID: {student.studentID}</p>
-    <p>Address: {student.address}</p>
-    <p>GPA: {student.gpa}</p>
-    <p>Grade: {student.grade}</p>
-    <p>Guardian 1: {student.firstNameFirstGuardian} {student.lastNameFirstGuardian}</p>
-    <p>Guardian 2: {student.firstNameSecondGuardian} {student.lastNameSecondGuardian}</p>
-    <p>Emergency Number: {student.emergencyNumber}</p>
-    <p>Enrollment Date: {student.enrollmentDate.split('T')[0]}</p> 
-    <div>
-    <NavLink to="/students/edit">
-    <Button style={{backgroundColor: '#D1913C', color:'#000', border: '0'}}>Edit Student</Button>
-    </NavLink>
-
-    </div> 
+  return (
+    <div style={{ marginLeft: "45%" }}>
+      <GlobalStyle />
+      <h2>Students</h2>
+      <div style={{ display: "grid", marginLeft: "-45%" }}>
+        {students &&
+          students.map((student) => {
+            return (
+              <div key={student.id}>
+                {student.isActive === 1 ? (
+                  <div
+                    style={{
+                      backgroundColor: "#fff",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginTop: "2%",
+                      height: "100%",
+                      width: "100%",
+                      maxWidth: "1000px",
+                      padding: "0 20px",
+                      borderRadius: "10px",
+                      boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.2)",
+                    }}
+                  >
+                    <Collapsible trigger={<h5>{student.name}</h5>} style={{ backgroundColor: "#fff" }}>
+                      <div
+                        style={{
+                          display: "grid",
+                          width: "100%",
+                          gridTemplateRows: "40px 40px 40px 60px 10px 30px",
+                          gridTemplateColumns: "400px 400px",
+                        }}
+                      >
+                        <p>Student ID: {student.studentID}</p>
+                        <p>Address: {student.address}</p>
+                        <p>GPA: {student.gpa}</p>
+                        <p>Grade: {student.grade}</p>
+                        <p>
+                          Guardian 1: {student.firstNameFirstGuardian} {student.lastNameFirstGuardian}
+                        </p>
+                        <p>
+                          Guardian 2: {student.firstNameSecondGuardian} {student.lastNameSecondGuardian}
+                        </p>
+                        <p>Emergency Number: {student.emergencyNumber}</p>
+                        <p>Enrollment Date: {student.enrollmentDate.split("T")[0]}</p>
+                        <div>
+                          <NavLink to="/students/edit">
+                            <Button style={{ backgroundColor: "#D1913C", color: "#000", border: "0" }}>
+                              Edit Student
+                            </Button>
+                          </NavLink>
+                        </div>
+                      </div>
+                    </Collapsible>
+                  </div>
+                ) : null}
+              </div>
+            );
+          })}
       </div>
-  </Collapsible>
+      <h2 style={{ marginTop: "4%" }}>Archived Students</h2>
+      <div style={{ display: "grid", marginLeft: "-45%" }}>
+    {students &&
+      students.map((student) => {
+        return (
+          <div key={student.id}>
+            {student.isActive === 0 ? (
+              <div
+                style={{
+                  backgroundColor: "#fff",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: "2%",
+                  height: "100%",
+                  width: "100%",
+                  maxWidth: "1000px",
+                  padding: "0 20px",
+                  borderRadius: "10px",
+                  boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.2)",
+                }}
+              >
+                <Collapsible trigger={<h5>{student.name}</h5>} style={{ backgroundColor: "#fff" }}>
+                  <div
+                    style={{
+                      display: "grid",
+                      width: "100%",
+                      gridTemplateRows: "40px 40px 40px 60px 10px 30px",
+                      gridTemplateColumns: "400px 400px",
+                    }}
+                  >
+                    <p>Student ID: {student.studentID}</p>
+                    <p>Address: {student.address}</p>
+                    <p>GPA: {student.gpa}</p>
+                    <p>Grade: {student.grade}</p>
+                    <p>
+                      Guardian 1: {student.firstNameFirstGuardian} {student.lastNameFirstGuardian}
+                    </p>
+                    <p>
+                      Guardian 2: {student.firstNameSecondGuardian} {student.lastNameSecondGuardian}
+                    </p>
+                    <p>Emergency Number: {student.emergencyNumber}</p>
+                    <p>Enrollment Date: {student.enrollmentDate.split("T")[0]}</p>
+                  </div>
+                </Collapsible>
+              </div>
+            ) : null}
+          </div>
+        );
+      })}
   </div>
-  :null}
-</td>
-</div>
-    })}
-</div> 
 
-
-<div style={{marginLeft:'-2%', marginTop: '2%'}}>
-<NavLink to="/students/create" activeStyle>
-    <Button>Create Student</Button>
-</NavLink>
+  <div style={{ marginLeft: "-2%", marginTop: "2%" }}>
+    <NavLink to="/students/create" activeStyle>
+      <Button>Create Student</Button>
+    </NavLink>
+  </div>
 </div>
-</div>     
+);
 }
 export default Students;
